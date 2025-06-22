08a6d92fd72561b2ba553b1447cb0a4d280b57be
yehonatna <yonidil1997@gmail.com>
2025-06-22 19:31:59 +0300

    add table

--- rps.js
+++ rps.js
@@ -24,24 +24,29 @@ function play(playerMove){
     document.getElementById("computerMovePic").src = computerMove + ".png";
     document.getElementById("playerMovePic").classList.remove("hide");
     document.getElementById("computerMovePic").classList.remove("hide");
+    let winner;
     if(playerMove == computerMove){
         document.getElementById("result").innerHTML = "Draw";
         draw++;
         document.getElementById("draw").innerHTML = draw;
+        winner = "Draw";
         bodyColorChange("bg-draw");
     }
     else if(playerMove == "Rock" && computerMove == "Scissors" || playerMove == "Paper" && computerMove == "Rock" || playerMove == "Scissors" && computerMove == "Paper"){
         document.getElementById("result").innerHTML = "Player win";
         playerScore++;
         document.getElementById("player_acore").innerHTML = playerScore;
+        winner = "Player";
         bodyColorChange("bg-player-win");
     }
     else {
         document.getElementById("result").innerHTML = "Computer wins";
         computerScore++;
         document.getElementById("computer_acore").innerHTML = computerScore;
+        winner = "Computer";
         bodyColorChange("bg-computer-win");
     }
+    addRowAndCollorChange(round, playerMove, computerMove, winner);
     precent();
 }
 function bodyColorChange(className){
@@ -55,6 +60,28 @@ function bodyColorChange(className){
 function resultColorChange(className){
     document.getElementById("result").classList.remove("bg-draw", "bg-player-win", "bg-computer-win");
     document.getElementById("result").classList.add(className);
+}
+function addRowAndCollorChange(round, playerMove, computerMove, winner){
+    const tableBody = document.getElementById("resultTableBody");
+    const newRow = document.createElement("tr");
+    var rowClass ="";
+    if (winner === "Draw") rowClass = "bg-draw";
+    else if (winner === "Player") rowClass = "bg-player-win";
+    else if (winner === "Computer") rowClass = "bg-computer-win";
+    newRow.classList.add(rowClass);    
+    newRow.innerHTML = `
+        <th>${round}</th>
+        <td>${playerMove}</td>
+        <td>${computerMove}</td>
+        <td>${winner}</td>
+    `;
+    tableBody.appendChild(newRow);
+    document.getElementById("resultTable").classList.remove("hide");
+    // גלילה אוטומטית לתחתית
+    const tableDiv = document.getElementById("result_table_div");
+    if (tableDiv) {
+        tableDiv.scrollTop = tableDiv.scrollHeight;
+    }
 }
 function precent(){
     var playerScore = Number(document.getElementById("player_acore").innerHTML);
